416c44d7d6c132c07108bc8ae1b8f366227bc1f6
ISMAIL IBIS <ibis.ismail@unistra.fr>
2016-01-27 12:22:41 +0100

    fix fixtures

--- mecc/apps/years/views.py
+++ mecc/apps/years/views.py
@@ -61,7 +61,346 @@ class UniversityYearListView(ListView):
 
 
 def initialize_year(request, code_year, template='years/initialize.html'):
+    to_process = [
+    {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "ART",
+        "field": "Arts, Lettres, Langues",
+        "label": "Facult\u00e9 des arts"
+    },
+    "pk": 1
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "LVI",
+        "field": "Arts, Lettres, Langues",
+        "label": "Facult\u00e9 des langues et des cultures \u00e9trang\u00e8res"
+    },
+    "pk": 2
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "LGA",
+        "field": "Arts, Lettres, Langues",
+        "label": "Facult\u00e9 des langues et sciences humaines appliqu\u00e9es (LSHA)"
+    },
+    "pk": 3
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "LET",
+        "field": "Arts, Lettres, Langues",
+        "label": "Facult\u00e9 des lettres"
+    },
+    "pk": 4
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "CPI",
+        "field": "Droit, \u00e9conomie, gestion et sciences politiques et sociales",
+        "label": "Centre d'\u00e9tudes internationales de la propri\u00e9t\u00e9 intellectuelle (CEIPI)"
+    },
+    "pk": 5
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "CUJ",
+        "field": "Droit, \u00e9conomie, gestion et sciences politiques et sociales",
+        "label": "Centre universitaire d'enseignement du journalisme (CUEJ)"
+    },
+    "pk": 6
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "EMS",
+        "field": "Droit, \u00e9conomie, gestion et sciences politiques et sociales",
+        "label": "Ecole de management Strasbourg (EM)"
+    },
+    "pk": 7
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "DRT",
+        "field": "Droit, \u00e9conomie, gestion et sciences politiques et sociales",
+        "label": "Facult\u00e9 de droit, de sciences politiques et de gestion"
+    },
+    "pk": 8
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "SEG",
+        "field": "Droit, \u00e9conomie, gestion et sciences politiques et sociales",
+        "label": "Facult\u00e9 de sciences \u00e9conomiques et de gestion (FSEG)"
+    },
+    "pk": 9
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "PAG",
+        "field": "Droit, \u00e9conomie, gestion et sciences politiques et sociales",
+        "label": "Institut de pr\u00e9paration \u00e0 l'administration g\u00e9n\u00e9rale (IPAG)"
+    },
+    "pk": 10
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "IEP",
+        "field": "Droit, \u00e9conomie, gestion et sciences politiques et sociales",
+        "label": "Institut d'\u00e9tudes politiques (IEP)"
+    },
+    "pk": 11
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "ODO",
+        "field": "Sant\u00e9",
+        "label": "Facult\u00e9 de chirurgie dentaire"
+    },
+    "pk": 12
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "MED",
+        "field": "Sant\u00e9",
+        "label": "Facult\u00e9 de m\u00e9decine"
+    },
+    "pk": 13
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "PHA",
+        "field": "Sant\u00e9",
+        "label": "Facult\u00e9 de pharmacie"
+    },
+    "pk": 14
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "EPE",
+        "field": "Sciences humaines et sociales",
+        "label": "Ecole sup\u00e9rieure du professorat et de l'\u00e9ducation (ESPE)"
+    },
+    "pk": 15
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "GEO",
+        "field": "Sciences humaines et sociales",
+        "label": "Facult\u00e9 de g\u00e9ographie et d'am\u00e9nagement"
+    },
+    "pk": 16
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "PLI",
+        "field": "Sciences humaines et sociales",
+        "label": "Facult\u00e9 de philosophie"
+    },
+    "pk": 17
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "PSY",
+        "field": "Sciences humaines et sociales",
+        "label": "Facult\u00e9 de psychologie"
+    },
+    "pk": 18
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "EDU",
+        "field": "Sciences humaines et sociales",
+        "label": "Facult\u00e9 de sciences de l'\u00e9ducation"
+    },
+    "pk": 19
+    } , {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "THC",
+        "field": "Sciences humaines et sociales",
+        "label": "Facult\u00e9 de th\u00e9ologie catholique"
+    },
+    "pk": 20
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "THP",
+        "field": "Sciences humaines et sociales",
+        "label": "Facult\u00e9 de th\u00e9ologie protestante"
+    },
+    "pk": 21
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "APS",
+        "field": "Sciences humaines et sociales",
+        "label": "Facult\u00e9 des sciences du sport"
+    },
+    "pk": 22
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "HIS",
+        "field": "Sciences humaines et sociales",
+        "label": "Facult\u00e9 des Sciences Historiques"
+    },
+    "pk": 23
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "SOC",
+        "field": "Sciences humaines et sociales",
+        "label": "Facult\u00e9 des sciences sociales"
+    },
+    "pk": 24
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "EOT",
+        "field": "Sciences, Technologies",
+        "label": "Ecole et observatoire des sciences de la Terre (EOST)"
+    },
+    "pk": 25
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "ECP",
+        "field": "Sciences, Technologies",
+        "label": "Ecole europ\u00e9enne de chimie, polym\u00e8res et mat\u00e9riaux (ECPM)"
+    },
+    "pk": 26
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "ESB",
+        "field": "Sciences, Technologies",
+        "label": "Ecole sup\u00e9rieure de biotechnologie de Strasbourg (ESBS)"
+    },
+    "pk": 27
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "CHM",
+        "field": "Sciences, Technologies",
+        "label": "Facult\u00e9 de chimie"
+    },
+    "pk": 28
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "VIE",
+        "field": "Sciences, Technologies",
+        "label": "Facult\u00e9 des sciences de la vie"
+    },
+    "pk": 29
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "IHA",
+        "field": "Sciences, Technologies",
+        "label": "Institut universitaire de technologie de Haguenau"
+    },
+    "pk": 30
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "ISC",
+        "field": "Sciences, Technologies",
+        "label": "Institut universitaire de technologie Louis Pasteur"
+    },
+    "pk": 31
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "ILL",
+        "field": "Sciences, Technologies",
+        "label": "Institut universitaire de technologie Robert Schuman"
+    },
+    "pk": 32
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "OBS",
+        "field": "Sciences, Technologies",
+        "label": "Observatoire astronomique de Strasbourg"
+    },
+    "pk": 33
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "ESP",
+        "field": "Sciences, Technologies",
+        "label": "T\u00e9l\u00e9com Physique Strasbourg"
+    },
+    "pk": 34
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "MAI",
+        "field": "Sciences, Technologies",
+        "label": "UFR de Math\u00e9matique et d'informatique"
+    },
+    "pk": 35
+    }, {
+    "model": "institute.institute",
+    "fields": {
+        "is_on_duty": True,
+        "code": "PHI",
+        "field": "Sciences, Technologies",
+        "label": "UFR physique et ing\u00e9nierie"
+    },
+    "pk": 36
+    }]
+
+    to_add = {
+        "dircomp": None,
+        "diretu": [],
+        "rac": None,
+        "scol_manager": [],
+    }
+
+    for e in to_process:
+        e['fields'].update(to_add)
     data = {}
+    print('---------------***********----------------')
+    print(to_process)
+
     try:
         y = UniversityYear.objects.get(code_year=code_year)
     except ObjectDoesNotExist:
