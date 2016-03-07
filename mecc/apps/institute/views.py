e7f0ac274d3f854a3c553844ec672206a8718bce
ISMAIL IBIS <ibis.ismail@unistra.fr>
2016-03-07 17:13:25 +0100

    institute list display : group by domaine

--- mecc/apps/institute/views.py
+++ mecc/apps/institute/views.py
@@ -216,8 +216,9 @@ class InstituteListView(ListView):
 
             for e in ordered_list:
                 iy = InstituteYear.objects.get(code_year=current_year, id_cmp=e.id)
+                print(e.field.name)
                 field = {
-                    'domaine': e.field,
+                    'domaine': e.field.name,
                     'label': e.label,
                     'code': e.code,
                     'dircomp': e.id_dircomp,
