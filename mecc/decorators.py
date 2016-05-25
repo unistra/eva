cea9838fd051f46e7c714a3b8ec1f8489d264bab
ISMAIL IBIS <ibis.ismail@unistra.fr>
2016-05-25 15:48:49 +0200

    create decorators

--- mecc/decorators.py
+++ mecc/decorators.py
@@ -1,6 +1,7 @@
 from functools import wraps
 # from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseForbidden
+from django.contrib.auth.models import Group
 
 
 def is_ajax_request(view_func):
@@ -21,8 +22,22 @@ def is_DES1(view_func):
     Check if user belong to DES1 group
     """
     @wraps(view_func)
+    def wrapper(request, group_name, *args, **kwargs):
+        group = Group.objects.get(name=group_name)
+        if group in request.user.groups.all():
+            return view_func(request, *args, **kwargs)
+        return HttpResponseForbidden("<h1>Forbidden</h1>You do not have \
+            permission to access this page.")
+    return wrapper
+
+
+def is_post_request(view_func):
+    """
+    Check if the request is POST
+    """
+    @wraps(view_func)
     def wrapper(request, *args, **kwargs):
-        if request.user.has_perm('unpaids.can_view_unpaid'):
+        if request.method == 'POST':
             return view_func(request, *args, **kwargs)
         return HttpResponseForbidden("<h1>Forbidden</h1>You do not have \
             permission to access this page.")
