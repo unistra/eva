26424a428fb466a5a918817288545d4522124360
ISMAIL IBIS <ibis.ismail@unistra.fr>
2016-05-27 17:04:19 +0200

    ask rule in use for selected year and correct degree

--- mecc/apps/utils/querries.py
+++ mecc/apps/utils/querries.py
@@ -1,9 +1,12 @@
 from mecc.apps.rules.models import Rule
+from mecc.apps.years.models import UniversityYear
 
 
 def rules_for_current_year(degree_type_code):
-    rules = Rule.objects.filter(
-        is_in_use=True).filter(degree_type__pk=degree_type_code)
+    rules = Rule.objects.filter(degree_type__pk=degree_type_code).filter(
+        code_year=UniversityYear.objects.get(
+            is_target_year=True).code_year).filter(is_in_use=True)
+
     if len(rules) > 0:
         return rules
     else:
