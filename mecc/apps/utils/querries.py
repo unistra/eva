0c5927bb423e2f5e2d3f47fd077912c19beb5ce6
ISMAIL IBIS <ibis.ismail@unistra.fr>
2016-05-30 17:35:39 +0200

    add docstring

--- mecc/apps/utils/querries.py
+++ mecc/apps/utils/querries.py
@@ -3,6 +3,10 @@ from mecc.apps.years.models import UniversityYear
 
 
 def rules_for_current_year(degree_type_code):
+    """
+    Return list of rules in use for current year applying to selected
+    degree type
+    """
     rules = Rule.objects.filter(degree_type__pk=degree_type_code).filter(
         code_year=UniversityYear.objects.get(
             is_target_year=True).code_year).filter(is_in_use=True)
@@ -14,7 +18,21 @@ def rules_for_current_year(degree_type_code):
 
 
 def rules_since_ever(degree_type_code):
+    """
+    Return list of rules applyint to selected degree type
+    """
     rules = Rule.objects.filter(degree_type__pk=degree_type_code)
+    if len(rules) > 0:
+        return rules
+    else:
+        return None
+
+
+def rules_for_selected_year(selected_year):
+    """
+    Return list of rules for selected year
+    """
+    rules = Rule.objects.filter(code_year=selected_year)
     if len(rules) > 0:
         return rules
     else:
