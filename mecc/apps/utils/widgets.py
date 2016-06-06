ea58165705b77050ded3fd9e17806dba01bb0d9d
ISMAIL IBIS <ibis.ismail@unistra.fr>
2016-06-06 14:33:18 +0200

    create custom widget for uploading file

---
+++ mecc/apps/utils/widgets.py
@@ -0,0 +1,36 @@
+from django.forms.widgets import ClearableFileInput
+from django.utils.translation import ugettext as _
+
+
+class CustomFileInput(ClearableFileInput):
+    template_with_initial = (
+        '<a href="%(initial_url)s">%(initial)s</a> </br>'
+        '%(clear_template)s<br/>'
+    )
+
+    def render(self, name, value, attrs=None):
+
+        substitutions = {
+        }
+        template = '%(input)s'
+        substitutions['input'] = super(ClearableFileInput, self).render(
+            name, value, attrs)
+        if self.is_initial(value):
+            template = self.template_with_initial
+            substitutions.update(self.get_template_substitution_values(value))
+            if not self.is_required:
+                substitutions['clear_template'] = self.template_with_clear % \
+                    substitutions
+        else:
+            template = (
+                '%(input)s</br> '
+                '%(add_pdf)s'
+            )
+            substitutions['add_pdf'] = '<button id="add_pdf" class="  \
+            pull-right">%s</button></br>' % _('Ajouter le document')
+        return (template % substitutions)
+
+    template_with_clear = (
+        '<button id="delete_pdf" class=" has-1em-margin-top"> \
+        %s</button></br>' % _('Supprimer le document')
+    )
