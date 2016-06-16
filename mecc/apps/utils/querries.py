085cb31e2a4166dde1c171ed33846b6353f1572c
ISMAIL IBIS <ibis.ismail@unistra.fr>
2016-06-16 15:16:17 +0200

    harmonize current_year querry

--- mecc/apps/utils/querries.py
+++ mecc/apps/utils/querries.py
@@ -1,21 +1,6 @@
 from mecc.apps.rules.models import Rule
 from mecc.apps.years.models import UniversityYear
 
-#
-# def rules_degree_for_year(degree_type_code):
-#     """
-#     Return list of rules in use for selected year applying to selected
-#     degree type
-#     """
-#     rules = Rule.objects.filter(degree_type__pk=degree_type_code).filter(
-#         code_year=UniversityYear.objects.get(
-#             is_target_year=True).code_year).filter(is_in_use=True)
-#
-#     if len(rules) > 0:
-#         return rules
-#     else:
-#         return None
-
 
 def rules_since_ever(degree_type_code):
     """
@@ -44,8 +29,11 @@ def rules_degree_for_year(degree_type_code, year):
     Return list of rules in use for selected year applying to selected
     degree type
     """
-    rules = rules_for_year(year).filter(
-        degree_type__pk=degree_type_code).filter(is_in_use=True)
+    r = rules_for_year(year)
+    if r is None:
+        return None
+
+    rules = r.filter(degree_type__pk=degree_type_code).filter(is_in_use=True)
 
     if len(rules) > 0:
         return rules
