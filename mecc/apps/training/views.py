b22111eaaac1c103312b3520a4c5d418516b2ca2
ISMAIL IBIS <ibis.ismail@unistra.fr>
2016-06-21 14:16:59 +0200

    add training createview

--- mecc/apps/training/views.py
+++ mecc/apps/training/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.list import ListView
+from django.views.generic import CreateView
 from .models import Training
+from .forms import TrainingForm
 
 
 class TrainingListView(ListView):
@@ -7,3 +9,12 @@ class TrainingListView(ListView):
     Training list view
     """
     model = Training
+
+
+class TrainingCreate(CreateView):
+    """
+    Training year create view
+    """
+    model = Training
+    success_url = '/years'
+    form_class = TrainingForm
