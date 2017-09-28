16cc1fc375918be2202991bcab169971a84afb46
Thomas Baguet <baguet@unistra.fr>
2017-09-28 16:15:20 +0200

    Enhance dashboard

--- mecc/apps/dashboards/views.py
+++ mecc/apps/dashboards/views.py
@@ -19,35 +19,41 @@ from django.http import JsonResponse
 
 def general_dashboard(request, template='dashboards/general_dashboard.html'):
     data = {}
+    supply_filter = []
+    cfvu_entries = []
     # objects needed
     uy = UniversityYear.objects.get(is_target_year=True)
     iy = InstituteYear.objects.filter(code_year=uy.code_year, date_expected_MECC__gt=uy.date_validation)
-    institutes = Institute.objects.filter(training__code_year=uy.code_year).distinct() #.annotate(training=Count('training__pk'))
-    institutes_cfvu = Institute.objects.filter(pk__in=iy.values('id_cmp'))
+    institutes = Institute.objects.filter(training__code_year=uy.code_year).distinct()
+
+    for year in iy:
+        inst = institutes.get(pk=year.id_cmp)
+        cfvu_entries.append(dict(domain=inst.field.name,cmp=inst.label,date=year.date_expected_MECC))
+
     trainings = Training.objects.filter(code_year=uy.code_year)
     trainings_eci = trainings.filter(MECC_type='E')
     trainings_cc_ct = trainings.filter(MECC_type='C')
+
+    # get institutes who are supplier for a training
+    for training in trainings:
+        if training.supply_cmp in institutes.values_list('code', flat=True):
+            supply_filter.append(training.supply_cmp)
+
+    supply_institutes = institutes.filter(code__in=supply_filter).distinct()
     doc_cadre = FileUpload.objects.get(object_id=uy.id)
     rules = Rule.objects.filter(code_year=uy.code_year).filter(is_edited__in=('O','X')).order_by('display_order')
-    # calculus
-    institutes_counter = institutes.count()
-    trainings_counter = trainings.count()
-    trainings_eci_counter = trainings_eci.count()
-    trainings_cc_ct_counter = trainings_cc_ct.count()
-    rules_counter = rules.count()
-    institutes_cfvu_counter = iy.count()
     # set datas for view
-    data['institutes_counter'] = institutes_counter
-    data['trainings_counter'] = trainings_counter
-    data['trainings_eci_counter'] = trainings_eci_counter
-    data['trainings_cc_ct_counter'] = trainings_cc_ct_counter
-    data['institutes'] = institutes
+    data['institutes_counter'] = supply_institutes.count()
+    data['trainings_counter'] = trainings.count()
+    data['trainings_eci_counter'] = trainings_eci.count()
+    data['trainings_cc_ct_counter'] = trainings_cc_ct.count()
+    data['institutes'] = supply_institutes
     data['rules'] = rules
-    data['rules_counter'] = rules_counter
+    data['rules_counter'] = rules.count()
     data['doc_cadre'] = doc_cadre
     data['university_year'] = uy
-    data['institutes_cfvu'] = institutes_cfvu
-    data['institutes_cfvu_counter'] = institutes_cfvu_counter
+    data['cfvu_entries'] = cfvu_entries
+    data['institutes_cfvu_counter'] = iy.count()
 
 
 
