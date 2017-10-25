34c55bb7e614ac16561599a519e92db63ffb4f10
ISMAIL IBIS <ibis.ismail@unistra.fr>
2017-10-25 11:58:18 +0200

    correctly dispatch for allmost all users

--- mecc/views.py
+++ mecc/views.py
@@ -12,21 +12,27 @@ def home(request):
     """
     root of all evil:
     dispatch according to user profile
-    """
+    """ 
     try:
         target_year = UniversityYear.objects.get(is_target_year=True)
         request.session['current_year'] = target_year.label_year
         request.session['current_code_year'] = target_year.code_year
     except ObjectDoesNotExist:
         pass
+    for e in request.user.groups.all():
+        if e.name == "VP":
+            return redirect('dashboards:general')
+        if e.name == "DES1":
+            return redirect('training:list_all')
     for e in request.user.meccuser.profile.all():
+        if e.code == "RESPFORM":
+            return redirect('training:list_resp')
         if e.code == 'REFAPP':
             return redirect('training:list', cmp=e.cmp)
         if e.code == 'DIRETU':
             return redirect('training:list', cmp=e.cmp)
         if e.code == "GESCOL":
             return redirect('training:list', cmp=e.cmp)
-    # if any(True for x in ['RESPFORM', 'DIRETU', 'GESCOL'] if x in [
-    #         e.code for e in request.user.meccuser.profile.all()]):
-    #     print([e.cmp for e in request.user.meccuser.profile.all()])
+        # if e.code == "ECI":
+        #     return redirect('')
     return render(request, 'base.html')
