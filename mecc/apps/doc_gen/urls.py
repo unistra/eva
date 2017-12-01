66ca49434db7114bf1869aa03519c9cef972169c
ISMAIL IBIS <ibis.ismail@unistra.fr>
2017-12-01 17:17:26 +0100

    add doc_generator wip

--- mecc/apps/doc_gen/urls.py
+++ mecc/apps/doc_gen/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from .views import home
+from .views import home, trainings_for_target
 
 urlpatterns = [
     url(r'^start/$', home, name='home'),
+    url(r'^trainings_for_target/$', trainings_for_target,
+        name='trainings_for_target'),
 ]
--- mecc/apps/doc_gen/views.py
+++ mecc/apps/doc_gen/views.py
@@ -6,7 +6,9 @@ from django.shortcuts import render,  redirect
 from django.http import JsonResponse
 
 from mecc.apps.institute.models import Institute
+from mecc.apps.mecctable.models import StructureObject
 from mecc.apps.utils.queries import currentyear
+from mecc.apps.training.models import Training
 from mecc.apps.years.models import UniversityYear, InstituteYear
 
 
@@ -15,10 +17,13 @@ def home(request, template='doc_generator/home.html'):
     Home screen for generator 3000
     """
     data = {}
-    all_institutes = Institute.objects.all()
+    current_year = currentyear().code_year
+    trainings = Training.objects.filter(code_year=current_year)
+    all_institutes = Institute.objects.filter(
+        code__in=[e.supply_cmp for e in trainings])
     profiles = request.user.meccuser.profile.all()
     institute_year = InstituteYear.objects.filter(
-        code_year=currentyear().code_year)
+        code_year=current_year)
     if request.user.is_superuser or 'DES1' in [
             e.name for e in request.user.groups.all()]:
         data['institutes'] = [(e, institute_year.get(id_cmp=e.id))
@@ -66,12 +71,18 @@ def home(request, template='doc_generator/home.html'):
             'label': _('Publication (MECC validées CFVU)'),
             'order': 8,
         }])
-        if 'DES1' in profiles_code or request.user.is_superuser :
+        if 'DES1' in profiles_code or request.user.is_superuser:
             target.append({
                 'code': "prepare_cfvu",
                 'label': _("Préparation CFVU"),
                 'order': 7,
             })
+            if request.user.is_superuser:
+                target.append({
+                    'code': "prepare_cc",
+                    'label': _("Préparation Conseil de composante"),
+                    'order': 5,
+                })
         else:
             target.append({
                 'code': "prepare_cc",
@@ -80,6 +91,105 @@ def home(request, template='doc_generator/home.html'):
             })
 
     data['university_year'] = UniversityYear.objects.get(
-        code_year=currentyear().code_year)
+        code_year=current_year)
     data['target'] = sorted(target, key=lambda k: k['order'])
+
+    # GET FIRST IN ORDER TO GET DISPLAY WHEN ARRIVING ON PAGE
+    try:
+        class FakeRequest(object):
+            """
+            Fake request to mock request GET
+            """
+            pass
+        fake_request = FakeRequest()
+        fake_request.GET = {
+            'target': data['target'][0].get('code'),
+            'institute': data['institutes'][0][0].code,
+            # ''
+        }
+        fake_request.user = request.user
+    except IndexError:
+        data['trainings'] = None
+        return render(request, template, data)
+
+    data['trainings'] = trainings_for_target(fake_request)
+
     return render(request, template, data)
+
+
+def trainings_for_target(request):
+    """
+    Return list of trainings according to target specificities, institute and
+    year
+    """
+    current_year = currentyear().code_year
+    target = request.GET.get('target')
+    institute = request.GET.get('institute')
+    json = True if request.GET.get('json') else False
+    trainings = Training.objects.filter(
+        code_year=current_year, institutes__code=institute)
+
+    def process_review_all():
+        return [e.small_dict for e in trainings]
+
+    def process_review_before():
+        return [e.small_dict for e in trainings if e.date_val_cmp in [
+            '', ' ', None]]
+
+    def process_review_after():
+        return [e.small_dict for e in trainings if e.date_val_cmp not in [
+            '', ' ', None]]
+
+    def process_review_my():
+        if request.user.is_superuser:
+            return [e.small_dict for e in trainings]
+        profiles = request.user.meccuser.profile.all()
+        struct_object = StructureObject.objects.filter(
+            code_year=current_year, RESPENS_id=request.user.username)
+        spe_trainings = []
+        if [e.cmp for e in profiles if e.code == 'RESPENS']:
+            spe_trainings.extend(
+                trainings.filter(code_year=current_year,
+                                 id__in=[e.owner_training_id for e in struct_object]))
+        if [e.cmp for e in profiles if e.code == 'RESPFORM']:
+            spe_trainings.extend(
+                trainings.filter(resp_formations=request.user.meccuser,
+                                 code_year=current_year))
+
+        return [e.small_dict for e in spe_trainings]
+
+    def process_prepare_cc():
+        return [e.small_dict for e in trainings if (
+            e.progress_rule == 'A' and e.progress_table == 'A')]
+
+    def process_prepare_cc_my():
+        print("I'm here : process_prepare_cc_my")
+        pass
+
+    def process_prepare_cfvu():
+        print("I'm here : process_prepare_cfvu")
+        pass
+
+    def process_publish_all():
+        print("I'm here : process_publish_all")
+        pass
+
+    def process_publish_my():
+        print("I'm here : process_publish_my")
+        pass
+
+    process = {
+        'review_all': process_review_all,
+        'review_before': process_review_before,
+        'review_after': process_review_after,
+        'review_my': process_review_my,
+        'prepare_cc': process_prepare_cc,
+        'prepare_cc_my': process_prepare_cc_my,
+        'prepare_cfvu': process_prepare_cfvu,
+        'publish_all': process_publish_all,
+        'publish_my': process_publish_my,
+    }
+
+    trains = process[target]()
+    print(trains)
+    return JsonResponse(trains, safe=False) if json else trains
