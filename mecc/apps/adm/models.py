4dee4238a99ed3ca2a1adc4d94cbab0eba3dcbf8
ISMAIL IBIS <ibis.ismail@unistra.fr>
2018-01-26 15:18:11 +0100

    reset user as before

--- mecc/apps/adm/models.py
+++ mecc/apps/adm/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Q
 from django.conf import settings
@@ -39,8 +39,8 @@ class MeccUser(models.Model):
         ('PROF', _('Enseignant')),
     )
     
-    django_user = settings.AUTH_USER_MODEL
-    user = models.OneToOneField(django_user, on_delete=models.CASCADE, related_name="mecc")
+    # django_user = settings.AUTH_USER_MODEL
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="mecc")
     cmp = models.CharField(_('Composante'), max_length=5, blank=True)
     status = models.CharField(
         _('Statut'), max_length=4, choices=STATUS_CHOICES, blank=True)
