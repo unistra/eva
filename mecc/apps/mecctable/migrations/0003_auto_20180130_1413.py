1f0605e866fb72ecaaa23a8e51494e5a79a71255
Thomas Baguet <baguet@unistra.fr>
2018-05-09 11:56:29 +0200

    Attempt to fix migrations files ep2

--- mecc/apps/mecctable/migrations/0003_auto_20180130_1413.py
+++ mecc/apps/mecctable/migrations/0003_auto_20180130_1413.py
@@ -4,16 +4,16 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 
 
-# class Migration(migrations.Migration):
-#
-#     dependencies = [
-#         ('mecctable', '0002_auto_20180130_1409'),
-#     ]
-#
-#     operations = [
-#         migrations.AlterField(
-#             model_name='exam',
-#             name='coefficient',
-#             field=models.DecimalField(null=True, verbose_name="Coefficient de l'épreuve", decimal_places=2, max_digits=4, blank=True),
-#         ),
-#     ]
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mecctable', '0002_auto_20180130_1409'),
+    ]
+
+    operations = [
+        # migrations.AlterField(
+        #     model_name='exam',
+        #     name='coefficient',
+        #     field=models.DecimalField(null=True, verbose_name="Coefficient de l'épreuve", decimal_places=2, max_digits=4, blank=True),
+        # ),
+    ]
