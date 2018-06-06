4d3dc53ee7a572923cc67d4081d091bb31de2e16
dbessey <daniel.bessey@unistra.fr>
2018-06-06 09:24:08 +0200

    Historique des MECC validées
    * #24 - Accès
    * #25 - Lettre de présentation
    * #26 - Autres documents

--- mecc/apps/doc_gen/urls.py
+++ mecc/apps/doc_gen/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
 from .views import home, trainings_for_target, available_target, \
-    preview_mecctable, dispatch_to_good_pdf, generate_pdf
+    preview_mecctable, dispatch_to_good_pdf, generate_pdf, history_home, \
+    history_for_year
 
 urlpatterns = [
     url(r'^start/$', home, name='home'),
@@ -15,4 +16,8 @@ urlpatterns = [
         name='dispatch'),
     url(r'^generate_pdf/$', generate_pdf,
         name='generate_pdf'),
+    url(r'^history/$', history_home,
+        name='history_home'),
+    url(r'^history/(?P<year>\d+)/$', history_for_year,
+        name='history_year'),
 ]
