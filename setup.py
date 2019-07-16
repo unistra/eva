8e4e85f4e2e923d6314f6e834a7c60d6076c96e7
dbessey <daniel.bessey@unistra.fr>
2019-07-16 13:38:32 +0200

    Bump version number to 2.3.14

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ recursive_requirements('requirements.txt', libraries, dependency_links)
 
 setup(
     name='mecc',
-    version='2.3.13',
+    version='2.3.14',
     packages=find_packages(),
     install_requires=libraries,
     dependency_links=dependency_links,
