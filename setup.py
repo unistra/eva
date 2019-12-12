38f830957b25583d56b88362ec65442d8863b849
dbessey <daniel.bessey@unistra.fr>
2019-12-12 11:48:09 +0100

    Bump version 2.4.3

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ recursive_requirements('requirements.txt', libraries, dependency_links)
 
 setup(
     name='mecc',
-    version='2.4.2',
+    version='2.4.3',
     packages=find_packages(),
     install_requires=libraries,
     dependency_links=dependency_links,
